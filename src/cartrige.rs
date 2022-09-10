7abfbe1de3d987b4c50cdad5d076b03df6c95ba9
Joshua Bourgeot <dragonsploder@gmail.com>
2022-09-09 23:20:55 -0400

    Implemented basic instruction outline

---
+++ src/cartrige.rs
@@ -0,0 +1,5 @@
+// Basicly mapper 0 right now
+
+struct Cart {
+    ROM: [u8, 0x8000] // 32 KB of ROM
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mainRam = Ram::new();
+    let mainCart = Cart::new();
+    mainCart.ROM[0x00] = 0xA9;
+    mainCart.ROM[0x01] = 0x07;
+
+    let mainBus = Bus::new(&mainRam, &mainCart);
+
+    let mainCpu = Cpu::new(&mainBus);
+
+    for i in 0..10 {
+        mainCpu.clock();
+    }
 }
