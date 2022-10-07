d1ad77eccbef294faf2b5a2098829786ace6b6ba
Ronan Maharaj <ronananakinmaharaj@gmail.com>
2022-10-06 23:24:27 -0400

    Wrote the load_file function

--- src/cartrige.rs
+++ src/cartrige.rs
@@ -1,4 +1,6 @@
 // Basicly mapper 0 right now
+use std::fs::File;
+use std::io::{prelude::*, Result};
 
 pub struct Cart {
     pub ROM: [u8; 0x8000] // 32 KB of ROM
@@ -7,5 +9,20 @@ pub struct Cart {
 impl Cart {
     pub fn new() -> Self {
         Self {ROM:[0; 0x8000]}
+    }
+
+
+    fn load_file(filename: &str) -> Result<Vec<u8>> {
+        let file = match File::open(filename) {
+            Err(why) => panic!("Couldn't open file: {}. Error: {}", filename, why), 
+            Ok(f) => f,
+        };
+        let size = match file.metadata() {
+            Err(why) => panic!("Couldn't open file: {}. Error: {}", filename, why), 
+            Ok(data) => data.len() as usize,
+        };
+        let buffer = vec![0; size];
+        file.read(&mut buffer)?;
+        Ok(buffer)
     }
 }
