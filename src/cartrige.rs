067eec0b750102bcb2e9694e86185bf46d0368dd
Ronan Maharaj <ronananakinmaharaj@gmail.com>
2022-10-06 23:37:57 -0400

    Wrote function for determining rom type

--- src/cartrige.rs
+++ src/cartrige.rs
@@ -2,13 +2,23 @@
 use std::fs::File;
 use std::io::{prelude::*, Result};
 
+enum RomType {
+    INES,
+    NES20,
+    Invalid
+}
+
 pub struct Cart {
-    pub ROM: [u8; 0x8000] // 32 KB of ROM
+    pub ROM: [u8; 0x8000], // 32 KB of ROM
+    pub r_type: RomType
 }
 
 impl Cart {
     pub fn new() -> Self {
-        Self {ROM:[0; 0x8000]}
+        Self {
+            ROM:[0; 0x8000],
+            r_type: RomType::Invalid
+        }
     }
 
 
@@ -24,5 +34,17 @@ impl Cart {
         let buffer = vec![0; size];
         file.read(&mut buffer)?;
         Ok(buffer)
+    }
+
+    fn determine_type(rom: &[u8]) -> RomType {
+        let i_nes = false;
+        let nes_20 = false;
+        if rom[0] == 'N' as u8 && rom[1] == 'E' as u8 && rom[2] == 'S' as u8 && rom[3] == 0x1A {
+            if rom[7] & 0x0C == 0x08 {
+                return RomType::NES20
+            }
+            return RomType::INES;
+        }
+        RomType::Invalid
     }
 }
